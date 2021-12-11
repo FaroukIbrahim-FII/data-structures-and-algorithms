9e9353e8bf93ba26c32b7b9675d75252b6077e05
faroukibrahim-fii <farouk94i@yahoo.com>
2021-12-11 20:37:27 +0200

    solving code challenge 30

---
+++ python/hashtable/test_hashtable.py
@@ -0,0 +1,42 @@
+from hashtable import HashTable
+import pytest
+
+
+def test_add_to_your_hash_table(hashtable):
+    expected = "55"
+    hashtable.add("FII","55")
+    actual = hashtable.get("FII")
+    assert actual == expected
+
+def test_retrieving_based_on_key(hashtable):
+    expected = "bee"
+    hashtable.add("bee","bee")
+    actual = hashtable.get("bee")
+    assert actual == expected
+
+def test_returns_null_for_a_non_exist_key(hashtable):
+    expected = None
+    actual = hashtable.get("ffffffff")
+    assert actual == expected
+
+
+def test_handle_collision_within_hash_table(hashtable):
+    expected = "55"
+    hashtable.add("FII","50")
+    hashtable.add("FII","55")
+    actual = hashtable.get("FII")
+    assert actual == expected
+
+def test_retrieving_value_within_a_hash_table_that_has_collision(hashtable):
+    expected = "55"
+    hashtable.add("FII","60")
+    hashtable.add("FII","55")
+    actual = hashtable.get("FII")
+    assert actual == expected
+
+
+###########Pytest Fixture#############
+
+@pytest.fixture
+def hashtable():
+	return HashTable()
